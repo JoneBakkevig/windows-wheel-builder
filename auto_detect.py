a4fbfbe0271db5b0040121f084ddb0a346dc2e30
Matthew Brett <matthew.brett@gmail.com>
2016-02-18 03:42:56 -0800

    More cleanup

--- auto_detect.py
+++ auto_detect.py
@@ -1,5 +1,6 @@
 import struct
-from cpuid import CPUID, cpu_vendor
+
+from cpuid import CPUID
 
 
 def have_cpuid(cpu):
@@ -80,6 +81,7 @@ FAM_MOD_EXTFAM = {
     (15, 0, 6): 'steamroller',
 }
 
+
 def cpu_detect(cpu):
     if not have_cpuid(cpu):
         return 'reference'
@@ -104,113 +106,6 @@ def cpu_detect(cpu):
     return ('reference' if (template in NEEDS_AVX and not have_avx)
             else template)
 
-
-"""
-    switch (family) {
-    case 0x6:
-      switch (extend_model) {
-      case 1:
-        switch (model) {
-        case 7:
-          //penryn uses dunnington config.
-          return CPUNAME_DUNNINGTON;
-        case 13:
-          return CPUNAME_DUNNINGTON;
-        }
-        break;
-      case 2:
-        switch (model) {
-        case 10:
-        case 13:
-          if(support_avx()) {
-            return CPUNAME_SANDYBRIDGE;
-          }else{
-            return CPUNAME_REFERENCE; //OS doesn't support AVX
-          }
-        }
-        break;
-      case 3:
-        switch (model) {
-        case 10:
-        case 14:
-          //Ivy Bridge
-          if(support_avx()) {
-            return CPUNAME_SANDYBRIDGE;
-          }else{
-            return CPUNAME_REFERENCE; //OS doesn't support AVX
-          }
-        case 12:
-        case 15:
-          //Haswell
-	case 13: //Broadwell
-          if(support_avx()) {
-            return CPUNAME_HASWELL;
-          }else{
-            return CPUNAME_REFERENCE; //OS doesn't support AVX
-          }
-
-        }
-        break;
-      case 4:
-        switch (model) {
-        case 5:
-        case 6:
-          //Haswell
-	case 7:
-	case 15:
-	  //Broadwell
-          if(support_avx()) {
-            return CPUNAME_HASWELL;
-          }else{
-            return CPUNAME_REFERENCE; //OS doesn't support AVX
-          }
-        }
-        break;
-      case 5:
-	switch (model) {
-	case 6:
-	  //Broadwell
-          if(support_avx()) {
-            return CPUNAME_HASWELL;
-          }else{
-            return CPUNAME_REFERENCE; //OS doesn't support AVX
-          }
-	}
-	break;
-      }
-      break;
-    }
-  }else if (vendor == VENDOR_AMD){
-    switch (family) {
-    case 0xf:
-      switch (extend_family) {
-      case 6:
-        switch (model) {
-        case 1:
-          if(support_avx())
-            return CPUNAME_BULLDOZER;
-          else
-            return CPUNAME_REFERENCE; //OS don't support AVX.
-        case 2:
-          if(support_avx())
-            return CPUNAME_PILEDRIVER;
-          else
-            return CPUNAME_REFERENCE; //OS don't support AVX.
-        case 0:
-          //Steamroller. Temp use Piledriver.
-          if(support_avx())
-            return CPUNAME_PILEDRIVER;
-          else
-            return CPUNAME_REFERENCE; //OS don't support AVX.
-        }
-      }
-      break;
-    }
-  }
-
-  return CPUNAME_REFERENCE;
-}
-"""
 
 cpu = CPUID()
 print(cpu_detect(cpu))
